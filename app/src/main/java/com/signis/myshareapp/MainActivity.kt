2f4a44b73afb457432a6e72c2cb226447832a2c5
Rochmad Sigit <rochmad.sigit@gmail.com>
2020-06-23 17:51:57 +0700

    Initial commit

---
+++ app/src/main/java/com/signis/myshareapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.signis.myshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            //Code
+            Log.i("MainActivity","Button was clicked!")
+
+            Toast.makeText(this ,"Button was clicked!",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
