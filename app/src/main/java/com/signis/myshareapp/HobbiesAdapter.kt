5a2627f94b90fc87aeb0aaa4b5964419b1bf6bd7
Rochmad Sigit <rochmad.sigit@gmail.com>
2020-06-25 18:31:10 +0700

    Section Three, Video Five

--- app/src/main/java/com/signis/myshareapp/HobbiesAdapter.kt
+++ app/src/main/java/com/signis/myshareapp/HobbiesAdapter.kt
@@ -1,16 +1,41 @@
 package com.signis.myshareapp
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list_item.view.*
 
 class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerView.Adapter<HobbiesAdapter.MyViewHolder> () {
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var currentHobby: Hobby? = null
+        var currentPosition: Int = 0
+
+        init {
+            itemView.setOnClickListener{
+                Toast.makeText(context, currentHobby!!.title + " Clicked!", Toast.LENGTH_SHORT).show()
+
+            }
+            itemView.imgShare.setOnClickListener{
+                val message: String = "My hobby is: " + currentHobby!!.title
+                val intent = Intent()
+                intent.action= Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT, message)
+                intent.type = "text/plain"
+
+                context.startActivity(Intent.createChooser(intent,"Please select App: "))
+            }
+        }
         fun setData(hobby: Hobby?, pos: Int){
             itemView.txvTitle.text = hobby!!.title
+
+            this.currentHobby = hobby
+            this.currentPosition = pos
 
         }
 
