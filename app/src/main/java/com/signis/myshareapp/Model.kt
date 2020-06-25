a0105c271621620cb3eef36768e71e5ddc436034
Rochmad Sigit <rochmad.sigit@gmail.com>
2020-06-25 17:54:15 +0700

    Section Three, Video Three

---
+++ app/src/main/java/com/signis/myshareapp/Model.kt
@@ -0,0 +1,36 @@
+package com.signis.myshareapp
+
+data class Hobby(var title: String)
+
+object Supplier {
+    val hobbies = listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking")
+    )
+}
